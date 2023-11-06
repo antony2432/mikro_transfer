c4aad28311bde4845ddc7c7535ff44689c6c9e5a
antony24 <achuquival@gmail.com>
2023-11-06 15:00:22 -0500

    formateando codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="mt-5 text-4xl text-center font-bold ">Hola mundo</h1>;
+    return <h1 className="mt-5 text-4xl text-center font-bold ">Hola mundo</h1>
 }
